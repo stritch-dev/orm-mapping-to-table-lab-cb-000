9392b6c5ed12eaf15321082688246129a4ff7298
stritch.dev <stritch.dev@gmail.com>
2018-03-19 13:11:45 -0500

    passed tests

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,40 @@
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  attr_accessor :name, :grade
+  attr_reader :id
   
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end 
+
+  def self.create_table
+    # replace spaces preceeded by pipe with empty string and
+    # replace carriage returns and new lines with a space
+    sql = <<-END_SQL.gsub(/^\s+\|/, '').gsub("[\r\n]", ' ')
+      | CREATE TABLE IF NOT EXISTS students (
+      |   id INTEGER PRIMARY KEY,
+      |   name TEXT, 
+      |   grade TEXT
+      | );
+    END_SQL
+    DB[:conn].execute sql
+  end 
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS students;"
+    DB[:conn].execute sql
+  end
+
+  def save
+         DB[:conn].execute("INSERT INTO students (name, grade) VALUES(:name, :grade)", "name" => @name, "grade" => @grade)
+         @id = DB[:conn].last_insert_row_id
+  end
+
+  def self.create(name:, grade:)
+    student = self.new(name, grade)   
+    student.save
+    student
+  end
 end
